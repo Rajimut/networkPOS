095ae3a913bcc6321cb660b9dd705be4fb49ca31
Raghavan Srinivasan <raghav983@gmail.com>
2015-02-17 22:51:28 -0800

    Added Seller options to input digital receipt. Not working. Need help understanding why :(

--- frontend/src/buyer_or_seller.js
+++ frontend/src/buyer_or_seller.js
@@ -1,18 +1,74 @@
 //Buyer or Seller
 var readline = require('readline');
 
-var rl = readline.createInterface({
+var choice = readline.createInterface({
 	input: process.stdin,
 	output: process.stdout
 });
 
-rl.question("Are you a Buyer or Seller?", function(answer) {
+choice.question("Are you a Buyer or Seller?\n", function(answer) {
+	console.log('\n');
 	if (answer == 'Buyer') {
-    	console.log('You are a Buyer')
+    	Buyer();
 	} else if (answer == 'Seller') {
-		console.log('You are a Seller')
+		choice.close();
+		Seller();
 	} else {
 		console.log('Take a hike!')
 	}
-	rl.close();
+	choice.close();
 });
+
+function Buyer() {
+	console.log('You are a Buyer\n');
+}
+
+function Seller() {
+	console.log('You are a Seller\n');
+	
+    var Total = 0;
+
+    var Sub_Total = 0;
+    var readline = require('readline');
+	var item_name = readline.createInterface({
+		input: process.stdin,
+		output: process.stdout
+	});
+    
+	item_name.question("Enter the name of the item: ", function(answer) {
+		if (answer == null) {
+			console.log('Error: Null item name.\n');
+			return 1;
+		}
+	});
+	//item_name.close();
+
+    /* var readline = require('readline');
+	var unit_price = readline.createInterface({
+		input: process.stdin,
+		output: process.stdout
+	});
+	unit_price.question("Enter unit price of the item: ", function(answer) {
+		if (answer == 0) {
+			console.log('Error: No unit price.\n');
+			return 1;
+		}
+	});
+	unit_price.close();
+	product_details.question("Enter the name of the item: ", function(quantity) {
+		if (quantity == 0) {
+			console.log('Error: Null item name.\n');
+			return 1;
+		}
+	});
+    Sub_Total = unit_price*quantity; 
+    console.log('    '+unit_price+'    '+quantity+'    '+Sub_Total);
+	Total = Total + Sub_Total;
+	console.log('Total: '+Total); */
+}
+
+
+
+
+
+
