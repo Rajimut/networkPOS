a3e257b01d179ae2315d91fb1d949de3ed7c3b15
Raghavan Srinivasan <raghav983@gmail.com>
2015-02-17 21:20:18 -0800

    Buyer or Seller option coded in Node.js

---
+++ frontend/src/buyer_or_seller.js
@@ -0,0 +1,11 @@
+//Buyer or Seller
+var question = 'Are you a Buyer or Seller?'
+console.log(question)
+
+var choice = require(choice)
+
+if (toString(choice) == 'Buyer') {
+    console.log('You are a Buyer')
+} else if (toString(choice) == 'Seller') {
+	console.log('You are a Seller')
+}
