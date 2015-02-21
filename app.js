9d19eb90099c741b9c34f55bb8e000e5a68ee70d
Muthu Raji <rajimut@gmail.com>
2015-02-20 19:56:03 -0600

    My first commit - raji :)

--- app.js
+++ app.js
@@ -19,6 +19,8 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// EDIT BY MUTHU RAJI
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
