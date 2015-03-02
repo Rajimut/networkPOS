56c780633e33c782cab51113b48fba441e9ad7b8
Muthu Raji <rajimut@gmail.com>
2015-03-02 00:45:04 -0600

    Added a  my receipts page

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
 
-    'url' : 'localhost:27025' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : 'localhost:27021' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 };
