711037a967439916aeeda27bfe63d81aa18e697e
Muthu Raji <rajimut@gmail.com>
2015-03-08 15:10:26 -0500

    POS terminal Fixed except minor bugs

--- routes/index.js
+++ routes/index.js
@@ -148,7 +148,7 @@ function isLoggedIn(req, res, next) {
 }
 /* GET Hello World page. */
 router.get('/helloworld', function(req, res) {
-    res.render('test', { title: 'Hello, World!' });
+    res.render('helloworld', { title: 'Hello, World!' });
 });
 
 
